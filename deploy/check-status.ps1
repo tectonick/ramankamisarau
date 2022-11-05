2776c1656f7545635a6d31ce672aa7f67781008c
tectonick <korn9509@gmail.com>
2022-11-05 22:23:30 +0400

    updated check status to check back, reduced sleep, fixed for build 404.html

--- deploy/check-status.ps1
+++ deploy/check-status.ps1
@@ -1,4 +1,6 @@
-echo "Checking site availability (give it 30 seconds)"
-Start-Sleep -Seconds 30
-$status = (curl nickkiselev.me).StatusCode;
-echo "Status code is $status"
+echo "Checking site availability (give it 20 seconds)"
+Start-Sleep -Seconds 20
+$statusfront = (curl nickkiselev.me).StatusCode;
+$statusback = (curl http://nickkiselev.me:1337).StatusCode;
+echo "Front status code is $statusfront"
+echo "Back status code is $statusback"
