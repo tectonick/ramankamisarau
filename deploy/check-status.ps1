1bd2bc838e15f4cb949c5f2fd7844a9bff8c6b9e
tectonick <korn9509@gmail.com>
2022-11-05 21:55:42 +0400

    Updated remote start scripts

--- deploy/check-status.ps1
+++ deploy/check-status.ps1
@@ -1,4 +1,4 @@
-echo "Checking site availability"
-Start-Sleep -Seconds 2
+echo "Checking site availability (give it 30 seconds)"
+Start-Sleep -Seconds 30
 $status = (curl nickkiselev.me).StatusCode;
 echo "Status code is $status"
--- deploy/restart.ps1
+++ deploy/restart.ps1
@@ -1,2 +1,2 @@
 echo 'Press enter to restart';
-ssh raman@nickkiselev.me "npm run --prefix ./ramankamisarau/ start &";
+ssh raman@nickkiselev.me "nohup npm run --prefix ./ramankamisarau/ start 1>/dev/null 2>/dev/null &";
