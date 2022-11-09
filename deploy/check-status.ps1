2a2f6ab164765e0790da988f2205c500c6eb8e9d
tectonick <korn9509@gmail.com>
2022-11-09 04:19:19 +0400

    retargeted config to Roma server

--- deploy/check-status.ps1
+++ deploy/check-status.ps1
@@ -1,6 +1,6 @@
 echo "Checking site availability (give it 40 seconds)"
 Start-Sleep -Seconds 40
-$statusfront = (curl nickkiselev.me).StatusCode;
-$statusback = (curl http://nickkiselev.me:1337).StatusCode;
+$statusfront = (curl http://46.101.212.75).StatusCode;
+$statusback = (curl http://46.101.212.75:1337).StatusCode;
 echo "Front status code is $statusfront"
 echo "Back status code is $statusback"
--- deploy/restart.ps1
+++ deploy/restart.ps1
@@ -1,2 +1,2 @@
 echo 'Press enter to restart';
-ssh raman@nickkiselev.me "nohup npm run --prefix ./ramankamisarau/ start 1>/dev/null 2>/dev/null &";
+ssh raman@46.101.212.75 "nohup npm run --prefix ./ramankamisarau/ start 1>/dev/null 2>/dev/null &";
