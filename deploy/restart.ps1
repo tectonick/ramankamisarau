57b1d2fb83eb1c01688ca45d63f3b5be39e61179
tectonick <korn9509@gmail.com>
2022-11-20 08:17:31 +0400

    updated pipeline

--- deploy/restart.ps1
+++ deploy/restart.ps1
@@ -1,2 +1,2 @@
 echo 'Press enter to restart';
-ssh raman@46.101.212.75 "nohup npm run --prefix ./ramankamisarau/ start 1>/dev/null 2>/dev/null &";
+ssh raman@46.101.212.75 "nohup npm run --prefix ./ramankamisarau/ prodrestart 1>/dev/null 2>/dev/null &";
