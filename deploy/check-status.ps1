fb532b5a8df48d968e8bd258a665a09fcb470395
tectonick <korn9509@gmail.com>
2022-11-20 06:42:45 +0400

    new ssl pipeline

--- deploy/check-status.ps1
+++ deploy/check-status.ps1
@@ -1,5 +1,5 @@
-echo "Checking site availability (give it 40 seconds)"
-Start-Sleep -Seconds 40
+echo "Checking site availability (give it 60 seconds)"
+Start-Sleep -Seconds 60
 $statusfront = (curl http://46.101.212.75).StatusCode;
 $statusback = (curl http://46.101.212.75:1337).StatusCode;
 echo "Front status code is $statusfront"
