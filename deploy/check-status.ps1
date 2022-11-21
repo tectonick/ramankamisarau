0bdf6dc4bd3298573498292a3bbed2071f12b2f8
tectonick <korn9509@gmail.com>
2022-11-22 01:49:27 +0400

    simplified pipeline, more timeout on animation

--- deploy/check-status.ps1
+++ deploy/check-status.ps1
@@ -1,5 +1,5 @@
-echo "Checking site availability (give it 60 seconds)"
-Start-Sleep -Seconds 60
+echo "Checking site availability (give it 80 seconds)"
+Start-Sleep -Seconds 80
 $statusfront = (curl http://46.101.212.75).StatusCode;
 $statusback = (curl http://46.101.212.75:1337).StatusCode;
 echo "Front status code is $statusfront"
--- deploy/init.ps1
+++
@@ -1,2 +0,0 @@
-echo 'Press enter to restart';
-ssh raman@46.101.212.75 "nohup npm run --prefix ./ramankamisarau/ start 1>/dev/null 2>/dev/null &";
