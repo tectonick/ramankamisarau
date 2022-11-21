7dc15780c93e74c9b2359b75e8777bd58c330835
tectonick <korn9509@gmail.com>
2022-11-22 01:32:52 +0400

    Animation and canonical

--- raman-front/src/Pages/Home.js
+++ raman-front/src/Pages/Home.js
@@ -1,27 +1,51 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import Concert from "../Components/Concert";
 import Article from "../Components/Article";
 import Api from "../Utility/api";
 
 function Home() {
-    const [LatestArticle, setLatestArticle] = useState({});
-    const [NearestConcert, setNearestConcert] = useState({});
+  const [LatestArticle, setLatestArticle] = useState({});
+  const [NearestConcert, setNearestConcert] = useState({});
 
-    useEffect(() => {
-        Api.getNearConcert().then((data)=>{
-            setNearestConcert(data);
-        })
-        Api.getLastArticle().then((data)=>{
-            setLatestArticle(data);
-        })
-    }, []);
+  useEffect(() => {
+    Api.getNearConcert().then((data) => {
+      setNearestConcert(data);
+    });
+    Api.getLastArticle().then((data) => {
+      setLatestArticle(data);
+    });
+  }, []);
   return (
     <div className="Home">
-        <h1>Raman Kamisarau</h1> 
-        <h2 id="pianist">Concert pianist based in Switzerland</h2>
-        {NearestConcert && <Concert supertitle="UPCOMING PERFORMANCE" title={NearestConcert.Title} place={NearestConcert.Place} description={NearestConcert.Description} date={new Date(NearestConcert.Date).toLocaleString()} link={"/concerts"}/>}
-        {LatestArticle && <Article supertitle="LATEST NEWS" title={LatestArticle.Title} description={LatestArticle.Description} date={new Date(LatestArticle.Date).toLocaleString()} link={"/news"}/>}
-        <iframe id="main-youtube-video" src="https://www.youtube.com/embed/xFHpvXL9Cvg" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+      <h1>Raman Kamisarau</h1>
+      <h2 id="pianist">Concert pianist based in Switzerland</h2>
+      {NearestConcert && (
+        <Concert
+          supertitle="UPCOMING PERFORMANCE"
+          title={NearestConcert.Title}
+          place={NearestConcert.Place}
+          description={NearestConcert.Description}
+          date={new Date(NearestConcert.Date).toLocaleString()}
+          link={"/concerts"}
+        />
+      )}
+      {LatestArticle && (
+        <Article
+          supertitle="LATEST NEWS"
+          title={LatestArticle.Title}
+          description={LatestArticle.Description}
+          date={new Date(LatestArticle.Date).toLocaleString()}
+          link={"/news"}
+        />
+      )}
+      <iframe
+        id="main-youtube-video"
+        src="https://www.youtube.com/embed/xFHpvXL9Cvg"
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      ></iframe>
     </div>
   );
 }
--- raman-front/src/Pages/Layout.js
+++ raman-front/src/Pages/Layout.js
@@ -1,15 +1,26 @@
 import * as React from "react";
-import { Outlet } from "react-router-dom";
-import Navbar from "../Components/Navbar"
+import { Outlet, useLocation } from "react-router-dom";
+import Navbar from "../Components/Navbar";
+import { SwitchTransition, CSSTransition } from "react-transition-group";
 
 function Layout() {
+  const location = useLocation();
+
   return (
     <div id="page-container">
       <Navbar />
 
-      <main>
-        <Outlet />
-      </main>
+      <SwitchTransition>
+        <CSSTransition
+          key={location.pathname}
+          timeout={100}
+          classNames="page"
+        >
+          <main className="page">
+            <Outlet />
+          </main>
+        </CSSTransition>
+      </SwitchTransition>
 
       <footer>© 2023 by Raman Kamisarau</footer>
     </div>
