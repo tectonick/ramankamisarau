3d6a2bfe3133b094e2940a2ddc78f682b4795cd4
Nickolay Kiselyov <n.kiselyov@itransition.com>
2024-02-07 15:56:41 +0400

    iso-like string should be adjusterd by a timezone offset

--- raman-front/src/Components/Concert.js
+++ raman-front/src/Components/Concert.js
@@ -1,57 +1,92 @@
-import React, { useState } from 'react';
-import { atcb_action } from 'add-to-calendar-button';
-import 'add-to-calendar-button/assets/css/atcb.css';
+import React, { useState } from "react";
+import { atcb_action } from "add-to-calendar-button";
+import "add-to-calendar-button/assets/css/atcb.css";
 import Config from "../Config/config";
 import DateHelpers from "../Utility/date";
 
-function Concert({supertitle, title, place, description, date, link}) {
-    const DateOptions = {
-        timeStyle: "short",
-        dateStyle: "medium"
-    };
-    const CollapseLengthThreshold = 300;
+function Concert({ supertitle, title, place, description, date, link }) {
+  const DateOptions = {
+    timeStyle: "short",
+    dateStyle: "medium",
+  };
+  const CollapseLengthThreshold = 300;
 
-    let startDateTime = new Date(date);
-    let endDateTime = DateHelpers.shiftDate(startDateTime, Config.Calendar.ConcertDuration);
+  let startDateTime = new Date(date);
+  let endDateTime = DateHelpers.shiftDate(
+    startDateTime,
+    Config.Calendar.ConcertDuration
+  );
 
-    // TODO Extract
-    let isCollapsible = description ? (description.length > CollapseLengthThreshold) : false;
-    const [Collapsed, setCollapsed] = useState(isCollapsible ? true : false);
+  // TODO Extract
+  let isCollapsible = description
+    ? description.length > CollapseLengthThreshold
+    : false;
+  const [Collapsed, setCollapsed] = useState(isCollapsible ? true : false);
 
-    function toggleCollapsed(e){
-        e.preventDefault();
-        setCollapsed(!Collapsed);
-    }
+  function toggleCollapsed(e) {
+    e.preventDefault();
+    setCollapsed(!Collapsed);
+  }
 
-    function add2calendar(e){
-        e.preventDefault();
+  function add2calendar(e) {
+    e.preventDefault();
 
-        atcb_action({
-            name: title,
-            startDate: DateHelpers.getISODate(startDateTime),
-            startTime: DateHelpers.getTimeHHMM(startDateTime),
-            endDate: DateHelpers.getISODate(endDateTime),
-            endTime: DateHelpers.getTimeHHMM(endDateTime),
-            description: description.substring(0,500),
-            options: ['Apple', 'Google', 'iCal', 'Microsoft365', 'Outlook.com', 'Yahoo'],
-            location:place,
-            timeZone: "Europe/Berlin",
-            iCalFileName: "RamanKamisarau-Concert",
-          });
-    }
+    atcb_action({
+      name: title,
+      startDate: DateHelpers.getISOLocalDate(startDateTime),
+      startTime: DateHelpers.getTimeHHMM(startDateTime),
+      endDate: DateHelpers.getISOLocalDate(endDateTime),
+      endTime: DateHelpers.getTimeHHMM(endDateTime),
+      description: description.substring(0, 500),
+      options: [
+        "Apple",
+        "Google",
+        "iCal",
+        "Microsoft365",
+        "Outlook.com",
+        "Yahoo",
+      ],
+      location: place,
+      timeZone: "Europe/Berlin",
+      iCalFileName: "RamanKamisarau-Concert",
+    });
+  }
 
-    return ( 
-        <div className="concert block">
-            {supertitle && <h2 className='supertitle'>{supertitle}</h2>}
-            <h2>{title}</h2>
-            <p className='date'>{startDateTime.toLocaleString("en-EN", DateOptions)}</p>
-            <div className='separator'/>
-            <p>{place}</p>
-            <div className={`html-content collapsible ${Collapsed ? 'collapsed': ''}`} dangerouslySetInnerHTML={{ __html: description }} />
-            {isCollapsible && <a href="" className='collapse-button' onClick={(e) => toggleCollapsed(e)}><img src="/img/expand.svg" alt=""></img>{Collapsed ? 'Expand' : 'Collapse'}</a>}
-            {link && <a href={link}><img src="/img/read.svg" alt=""></img>Read more</a>}{<a onClick={(e)=>add2calendar(e)} href=""><img src="/img/calendar.svg" alt=""></img>Add to calendar</a>}
-        </div>
-     );
+  return (
+    <div className="concert block">
+      {supertitle && <h2 className="supertitle">{supertitle}</h2>}
+      <h2>{title}</h2>
+      <p className="date">
+        {startDateTime.toLocaleString("en-EN", DateOptions)}
+      </p>
+      <div className="separator" />
+      <p>{place}</p>
+      <div
+        className={`html-content collapsible ${Collapsed ? "collapsed" : ""}`}
+        dangerouslySetInnerHTML={{ __html: description }}
+      />
+      {isCollapsible && (
+        <a
+          href=""
+          className="collapse-button"
+          onClick={(e) => toggleCollapsed(e)}
+        >
+          <img src="/img/expand.svg" alt=""></img>
+          {Collapsed ? "Expand" : "Collapse"}
+        </a>
+      )}
+      {link && (
+        <a href={link}>
+          <img src="/img/read.svg" alt=""></img>Read more
+        </a>
+      )}
+      {
+        <a onClick={(e) => add2calendar(e)} href="">
+          <img src="/img/calendar.svg" alt=""></img>Add to calendar
+        </a>
+      }
+    </div>
+  );
 }
 
 export default Concert;
--- raman-front/src/Utility/date.js
+++ raman-front/src/Utility/date.js
@@ -1,15 +1,19 @@
 const DateHelpers = {
-    shiftDate(dateTime, ms){
-        return new Date(dateTime.getTime() + ms);
-    }
-    ,
-    getTimeHHMM(dateTime){
-        return `${dateTime.getHours().toString().padStart(2,0)}:${dateTime.getMinutes().toString().padStart(2,0)}`;
-    }
-    ,
-    getISODate(dateTime){
-        return dateTime.toISOString().substring(0,10);
-    }
-}
+  shiftDate(dateTime, ms) {
+    return new Date(dateTime.getTime() + ms);
+  },
+  getTimeHHMM(dateTime) {
+    return `${dateTime.getHours().toString().padStart(2, 0)}:${dateTime
+      .getMinutes()
+      .toString()
+      .padStart(2, 0)}`;
+  },
+  getISOLocalDate(dateTime) {
+    const offset = dateTime.getTimezoneOffset();
+    const dateLocal = new Date(dateTime.getTime() - offset * 60 * 1000);
+
+    return dateLocal.toISOString().substring(0, 10);
+  },
+};
 
 export default DateHelpers;
